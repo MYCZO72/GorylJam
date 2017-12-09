7e936a55fb6450fda43992825067b276a271697f
kiczak <szymonkot1@gmail.com>
2017-12-09 12:56:08 +0100

    building skrypt

---
+++ GorylGra/Assets/Scripts/Building.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Random = UnityEngine.Random;
+
+public class Building : MonoBehaviour
+{
+	public int height = 5;
+
+	private Transform buildingHolder = null;
+	public GameObject[] floors;
+
+	private List<GameObject> pietra = new List<GameObject>();
+
+	void Create()
+	{
+		if( buildingHolder == null )
+			buildingHolder = new GameObject("Building").transform;
+
+		for(int i = 1; i <= height; i++)
+		{
+			GameObject nowy = floors[ Random.Range( 0, floors.Length - 1 ) ];
+
+			GameObject instance = Instantiate( nowy, new Vector3( 0f, i * 3, 0f ), Quaternion.identity, buildingHolder );
+
+			//instance.transform.SetParent( buildingHolder );
+
+			pietra.Add( instance );
+		}
+	}
+
+	void DestroyTop()
+	{
+		GameObject top = pietra[ pietra.Count - 1 ];
+		pietra.RemoveAt( pietra.Count - 1 );
+
+		Destroy( top );
+		height --;
+	}
+
+	void Start()
+	{
+		Create();
+	}
+}
---
+++ GorylGra/Assets/Scripts/Floor.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Floor : MonoBehaviour {
+
+	Rigidbody2D rigidbody;
+	private Transform pozycja;
+
+
+	void Start()
+	{
+		rigidbody = GetComponent<Rigidbody2D>();
+		pozycja = GetComponent<Transform>();
+	}
+}
